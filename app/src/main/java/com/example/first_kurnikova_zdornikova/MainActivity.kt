908417247e8a84c3298317ffe6e7a83873f3bdf3
ginmork <wirlzakz@gmail.com>
2026-01-24 13:42:50 +0300

    2

--- app/src/main/java/com/example/first_kurnikova_zdornikova/MainActivity.kt
+++ app/src/main/java/com/example/first_kurnikova_zdornikova/MainActivity.kt
@@ -1,20 +1,25 @@
 package com.example.first_kurnikova_zdornikova
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import kotlin.random.Random
+import com.example.first_kurnikova_zdornikova.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        binding.buttonRes.setOnClickListener {
+            val randomNumber = Random.nextInt(1, 7)
+            binding.textViewRes.text = "Выпало $randomNumber"
         }
     }
 }
